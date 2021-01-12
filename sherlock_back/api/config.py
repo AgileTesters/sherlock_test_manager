014f9ecc89e34b0e862ebf21c35256e03ab2ed98
Ramses Almeida <ramsesalmeida@bemol.com.br>
2021-01-12 10:43:10 -0300

    little manager on the config

--- sherlock_back/api/config.py
+++ sherlock_back/api/config.py
@@ -2,10 +2,24 @@
 """Configuration params for sherlock."""
 import os
 import secrets
-
 basedir = os.path.abspath(os.path.dirname(__file__))
-CORS_HEADER = 'Content-Type'
-TOKEN_TIMEOUT = 99999
-SECRET_KEY = secrets.token_urlsafe(20)
-SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
-SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+class Config(object):
+    DEBUG = False
+    TESTING = False
+    CORS_HEADER = 'Content-Type'
+    TOKEN_TIMEOUT = 99999
+    SECRET_KEY = secrets.token_urlsafe(20)
+    SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+class Production(Config):
+    DEBUG = False
+
+class DevelopmentConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    TESTING = True
