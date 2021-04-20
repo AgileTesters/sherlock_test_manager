b64aae8f2cfaba3a99c9eeb2ae7ca2083d0dd47e
Leonardo Galani <leo@leonardobg.com.br>
2021-04-20 12:34:58 -0300

    placeholder

---
+++ sherlock_frontend/src/components/Project/ProjectHeader.vue
@@ -0,0 +1,140 @@
+<template>
+	<div
+		class="tile is-ancestor dashboard_default"
+		style="margin-top:30px; width:95%; margin:auto"
+	>
+		<div class="tile is-4 is-vertical is-parent" style="max-height: 100px;">
+			<div class="tile is-child box sherlock_tiles">
+				<p style="font-size: 7px">project name</p>
+				<p class="title">{{ projectName }}</p>
+			</div>
+		</div>
+		<div class="tile is-parent is-vertical">
+			<div class="tile is-child box sherlock_tiles">
+				<nav class="navbar" role="navigation" aria-label="main navigation">
+					<div id="navbarSherlock" class="navbar-menu">
+						<div class="navbar-end">
+							<a class="navbar-item">
+								<i
+									class="fal fa-home"
+									style="text-align: center;font-size: x-large;"
+								>
+									<br />
+									<span
+										style="font-size:15px; font-family: Nunito; text-align: center;"
+									>
+										Home</span
+									>
+								</i>
+							</a>
+							<div class="navbar-item has-dropdown is-hoverable">
+								<a class="navbar-link">
+									<i
+										class="fal fa-users-cog"
+										style="text-align: center;font-size: x-large;"
+									>
+										<br />
+										<span
+											style="font-size:15px; font-family: Nunito; text-align: center;"
+											>Teams</span
+										>
+									</i>
+								</a>
+								<div class="navbar-dropdown is-right">
+									<a class="navbar-item">
+										TO BO IMPLEMENTED
+									</a>
+									<hr class="navbar-divider" />
+									<a class="navbar-item">
+										x
+									</a>
+								</div>
+							</div>
+
+							<div class="navbar-item has-dropdown is-hoverable">
+								<a class="navbar-link">
+									<i
+										class="fal fa-desktop-alt"
+										style="text-align: center;font-size: x-large;"
+									>
+										<br />
+										<span
+											style="font-size:15px; font-family: Nunito; text-align: center;"
+											>Projects</span
+										>
+									</i>
+								</a>
+								<div class="navbar-dropdown is-right">
+									<router-link class="navbar-item" :to="{ path: '/dashboard' }">
+										All projects
+									</router-link>
+									<hr class="navbar-divider" />
+									<createProjectModal />
+								</div>
+							</div>
+
+							<div class="navbar-item has-dropdown is-hoverable">
+								<a class="navbar-link">
+									<i
+										class="fal fa-address-card"
+										style="text-align: center;font-size: x-large;"
+										><br />
+										<span
+											style="font-size:15px; font-family: Nunito; text-align: center;"
+											>Profile</span
+										>
+									</i>
+								</a>
+								<div class="navbar-dropdown is-right">
+									<a class="navbar-item">
+										Profile
+									</a>
+									<a class="navbar-item" @click="logout">
+										Logout
+									</a>
+									<hr class="navbar-divider" />
+									<a
+										class="navbar-item"
+										href="https://github.com/AgileTesters/sherlock_test_manager/issues"
+									>
+										Report an issue
+									</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</nav>
+			</div>
+		</div>
+	</div>
+</template>
+<script>
+export default {
+	props: {
+		projectId: Number,
+		projectName: String,
+		state: null
+	},
+	data() {
+		return {
+			caseState: this.state
+		};
+	},
+	methods: {
+		approve() {
+			this.caseState = "approved";
+		},
+		reprove() {
+			this.caseState = "reproved";
+		},
+		block() {
+			this.caseState = "blocked";
+		},
+		resetStatus() {
+			this.caseState = null;
+		}
+	},
+	mounted() {}
+};
+</script>
+<style></style>
--- sherlock_frontend/src/views/CaseExecution.vue
+++ sherlock_frontend/src/views/CaseExecution.vue
@@ -1,95 +1,100 @@
 <template>
-  <navBar />
+	<navBar />
 
-  <div class="tile is-ancestor" style="margin-top:30px; width:95%; margin:auto">
-    <div class="tile is-parent is-vertical">
-      <div class="tile is-child box case_tile">
-        <div class="has-text-left" v-if="project">
-          <p style="font-size: 7px;">
-            project name:
-          </p>
-            <router-link class="title" :to="{ name: 'projectDashboard', params: { projectId: projectId }}">
-              {{ project.name }}
-            </router-link>
-        </div>
-        <table class="table is-fullwidth" v-if="cases.length > 0">
-          <thead>
-            <tr>
-              <th>
-                <input type="checkbox" />
-              </th>
-              <th></th>
-              <th>
-                <span>
-                  cases
-                </span>
-              </th>
-              <th style="text-align: right;">
-                <span>
-                  <attachModal
-                    v-bind:casesSelected="casesSelected"
-                    v-bind:cases="cases"
-                    v-bind:projectId="projectId"
-                  />
-                </span>
-              </th>
-            </tr>
-          </thead>
-          <tbody v-for="item in cases" :key="item.exhibition_order">
-            <tr>
-              <td>
-                <input type="checkbox" :value="item" v-model="casesSelected" />
-              </td>
-              <td class="col_id">
-                <span>{{ item.id }}</span>
-              </td>
-              <td>
-                <VueMarkdownIt
-                  class="content"
-                  :typographer="true"
-                  :source="item.name"
-                  :breaks="true"
-                  :html="true"
-                  :plugins="plugins"
-                />
-              </td>
-              <td style="text-align: right;"><actionCase /></td>
-            </tr>
-            <tr
-              v-for="sub_item in item.child_cases"
-              :key="sub_item.exhibition_order"
-            >
-              <td>
-                <input
-                  type="checkbox"
-                  :value="sub_item"
-                  v-model="casesSelected"
-                />
-              </td>
-              <td class="col_id">
-                <span>{{ item.id }}</span
-                >-<span>{{ sub_item.exhibition_order }}</span>
-              </td>
-              <td>
-                <div class="columns">
-                  <div class="column is-1"></div>
-                  <div class="column">
-                    <VueMarkdownIt
-                      class="content"
-                      :typographer="true"
-                      :source="sub_item.name"
-                      :breaks="true"
-                      :html="true"
-                      :plugins="plugins"
-                    />
-                  </div>
-                </div>
-              </td>
-              <td style="text-align: right !important;"><actionCase /></td>
-            </tr>
-          </tbody>
-        </table>
-        <!-- <nav
+	<div v-if="project">
+		<projectHeader :projectName="project.name" :projectId="project.id" />
+	</div>
+
+	<div class="tile is-ancestor" style="margin-top:30px; width:95%; margin:auto">
+		<div class="tile is-parent is-vertical">
+			<div class="tile is-child box case_tile">
+				<table class="table is-fullwidth" v-if="cases.length > 0">
+					<thead style="font-size:10px">
+						<tr>
+							<th>
+								<input type="checkbox" />
+							</th>
+							<th>
+								<span>
+									id
+								</span>
+							</th>
+							<th>
+								<span>
+									cases
+								</span>
+							</th>
+							<th style="text-align: right">
+								<span>
+									actions
+								</span>
+							</th>
+							<th style="text-align: right;">
+								<span>
+									<attachModal
+										v-bind:casesSelected="casesSelected"
+										v-bind:cases="cases"
+										v-bind:projectId="projectId"
+									/>
+								</span>
+							</th>
+						</tr>
+					</thead>
+					<tbody v-for="item in cases" :key="item.exhibition_order">
+						<tr>
+							<td>
+								<input type="checkbox" :value="item" v-model="casesSelected" />
+							</td>
+							<td class="col_id">
+								<span>{{ item.id }}</span>
+							</td>
+							<td>
+								<VueMarkdownIt
+									class="content"
+									:typographer="true"
+									:source="item.name"
+									:breaks="true"
+									:html="true"
+									:plugins="plugins"
+								/>
+							</td>
+							<td style="text-align: right;"><actionCase /></td>
+						</tr>
+						<tr
+							v-for="sub_item in item.child_cases"
+							:key="sub_item.exhibition_order"
+						>
+							<td>
+								<input
+									type="checkbox"
+									:value="sub_item"
+									v-model="casesSelected"
+								/>
+							</td>
+							<td class="col_id">
+								<span>{{ item.id }}</span
+								>-<span>{{ sub_item.exhibition_order }}</span>
+							</td>
+							<td>
+								<div class="columns">
+									<div class="column is-1"></div>
+									<div class="column">
+										<VueMarkdownIt
+											class="content"
+											:typographer="true"
+											:source="sub_item.name"
+											:breaks="true"
+											:html="true"
+											:plugins="plugins"
+										/>
+									</div>
+								</div>
+							</td>
+							<td style="text-align: right !important;"><actionCase /></td>
+						</tr>
+					</tbody>
+				</table>
+				<!-- <nav
           class="pagination is-small"
           role="navigation"
           aria-label="pagination"
@@ -117,120 +122,122 @@
             <li><a class="pagination-link" aria-label="Goto page 86">86</a></li>
           </ul>
         </nav> -->
-        <div v-else class="has-text-centered" style="margin">
-          <i class="fal fa-cat-space" style="font-size:50px"></i><br />
-          <p class="is-family-monospace	is-size-7">
-            So much empty space.... And I think you should not be here :)
-          </p>
-        </div>
-      </div>
-    </div>
-  </div>
+				<div v-else class="has-text-centered" style="margin">
+					<i class="fal fa-cat-space" style="font-size:50px"></i><br />
+					<p class="is-family-monospace	is-size-7">
+						So much empty space.... And I think you should not be here :)
+					</p>
+				</div>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
 import axios from "axios";
 
 import NavBar from "@/components/InternalNavBar";
+import ProjectHeader from "@/components/Project/ProjectHeader";
 import ActionCase from "@/components/Cycle/CaseAction";
 
 import VueMarkdownIt from "vue3-markdown-it";
 import MarkdownHighligh from "markdown-it-highlightjs";
 
 export default {
-  components: {
-    navBar: NavBar,
-    actionCase: ActionCase,
-    VueMarkdownIt
-  },
-  data() {
-    return {
-      projectId: this.$route.params.projectId,
-      cases: [],
-      casesSelected: [],
-      newCase: "",
-      loading: false,
-      tag_field: "",
-      error: null,
-      project: null,
-      plugins: [
-        {
-          plugin: MarkdownHighligh
-        }
-      ]
-    };
-  },
-  methods: {
-    addNewCase() {
-      if (this.newCase === "") {
-        console.log("blank test case");
-      } else {
-        const payload = {
-          test_case_text: this.newCase,
-          project_id: this.projectId,
-          parent_id: "0"
-        };
-        const requestOptions = {
-          method: "post",
-          url: "/cases/new",
-          data: payload
-        };
-        axios(requestOptions)
-          .then(response => {
-            // TODO: add visual feedback that is working
-            if (response.data.message != "CASE_CREATED") {
-              this.error = response.data.message;
-            } else {
-              this.fetchCases();
-              this.newCase = "";
-            }
-          })
-          .catch(error => {
-            this.error = error;
-          });
-      }
-    },
-    fetchCases() {
-      var requestOptions = {
-        method: "get",
-        url: "/cases/find_by_project/" + this.projectId
-      };
-      axios(requestOptions)
-        .then(response => {
-          this.cases = response.data;
-        })
-        .catch(error => {
-          this.error = error;
-        });
-    },
-    fetchProjectDetails() {
-      var requestOptions = {
-        method: "get",
-        url: "/projects/project/" + this.projectId
-      };
-      axios(requestOptions)
-        .then(response => {
-          this.project = response.data;
-        })
-        .catch(error => {
-          this.error = error;
-        });
-    }
-  },
-  async mounted() {
-    await this.fetchCases();
-    await this.fetchProjectDetails();
-  }
+	components: {
+		navBar: NavBar,
+		projectHeader: ProjectHeader,
+		actionCase: ActionCase,
+		VueMarkdownIt
+	},
+	data() {
+		return {
+			projectId: this.$route.params.projectId,
+			cases: [],
+			casesSelected: [],
+			newCase: "",
+			loading: false,
+			tag_field: "",
+			error: null,
+			project: null,
+			plugins: [
+				{
+					plugin: MarkdownHighligh
+				}
+			]
+		};
+	},
+	methods: {
+		addNewCase() {
+			if (this.newCase === "") {
+				console.log("blank test case");
+			} else {
+				const payload = {
+					test_case_text: this.newCase,
+					project_id: this.projectId,
+					parent_id: "0"
+				};
+				const requestOptions = {
+					method: "post",
+					url: "/cases/new",
+					data: payload
+				};
+				axios(requestOptions)
+					.then(response => {
+						// TODO: add visual feedback that is working
+						if (response.data.message != "CASE_CREATED") {
+							this.error = response.data.message;
+						} else {
+							this.fetchCases();
+							this.newCase = "";
+						}
+					})
+					.catch(error => {
+						this.error = error;
+					});
+			}
+		},
+		fetchCases() {
+			var requestOptions = {
+				method: "get",
+				url: "/cases/find_by_project/" + this.projectId
+			};
+			axios(requestOptions)
+				.then(response => {
+					this.cases = response.data;
+				})
+				.catch(error => {
+					this.error = error;
+				});
+		},
+		fetchProjectDetails() {
+			var requestOptions = {
+				method: "get",
+				url: "/projects/project/" + this.projectId
+			};
+			axios(requestOptions)
+				.then(response => {
+					this.project = response.data;
+				})
+				.catch(error => {
+					this.error = error;
+				});
+		}
+	},
+	async mounted() {
+		await this.fetchCases();
+		await this.fetchProjectDetails();
+	}
 };
 </script>
 <style>
 .child_cases {
-  background-color: #eff1f3;
+	background-color: #eff1f3;
 }
 
 .col_id {
-  vertical-align: middle !important;
-  font-size: 09px;
-  width: 60px;
+	vertical-align: middle !important;
+	font-size: 09px;
+	width: 60px;
 }
 </style>
